035d7b552a71f5f2f75d693064f8b65e195a4db1
Colebot <70764431+colebot17@users.noreply.github.com>
2026-04-13 20:55:14 -0400

    Log new mode

--- server.js
+++ server.js
@@ -57,6 +57,7 @@ loadMode();
 
 app.post("/setMode", async (req, res) => {
     mode = req.body.mode;
+    console.log("Set mode to", mode);
     broadcast({ "type": "modeChange" });
 
     switch (req.body.mode) {
